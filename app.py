d8ad1f5825c60e4c84ae1758e0c70016208ca79b
rkdwjdghk <rkdwjdghk33@gmail.com>
2026-04-15 10:06:34 +0900

    1.0.0.1

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import html
 st.set_page_config(page_title="유럽상품1팀 인솔자 출장비 계산기", layout="centered")
 
 st.title("유럽상품1팀 인솔자 출장비 계산기")
+st.caption("ver 1.0.0.1")
+st.divider()
 
 # 세션 상태 초기화
 if "show_note_form" not in st.session_state:
@@ -48,22 +50,12 @@ people_count = st.number_input("인솔자 제외 인원수", min_value=1, step=1
 # 식사 불포함 횟수
 meal_excluded_count = st.number_input("식사 불포함 횟수 입력", min_value=0, step=1)
 
-# 프라하 야간투어 포함 여부
+# 체크박스 순서
 prague_night_tour = st.checkbox("프라하 야간투어 스페셜 포함 상품인가요?")
-
-# 귀국편 출발 시간 20시 이후 여부
-return_after_20 = st.checkbox("귀국편 출발 시간이 20시 이후인가요?")
-
-# 출발편 시간 오전 05:59 이전 여부
-departure_before_559 = st.checkbox("출발편 시간이 오전 05:59 이전인가요?")
-
-# 비엔나 음악회 스페셜 포함 여부
 vienna_special = st.checkbox("비엔나 음악회 스페셜 포함 상품인가요?")
-
-# 스타 인솔자 상품 여부
+return_after_20 = st.checkbox("귀국편 출발 시간이 20시 이후인가요?")
+departure_before_559 = st.checkbox("출발편 시간이 오전 05:59 이전인가요?")
 star_guide = st.checkbox("스타 인솔자 상품인가요?")
-
-# 발칸 방문 여부
 balkan_visit = st.checkbox("발칸 방문 여부")
 
 # 발칸 방식 적용 여부
@@ -177,6 +169,10 @@ if st.button("계산하기"):
                 if is_special_balkan_code:
                     messages.append(f"상품코드 {code_prefix_6}는 발칸 방식으로 자동 적용되었습니다.")
 
+                if grade == "프리미엄":
+                    extra_amount += 200000
+                    formula_parts.append("(프리미엄 200,000원)")
+
             # 3. 일반 방식
             else:
                 if applied_people_count >= 20:
@@ -186,6 +182,10 @@ if st.button("계산하기"):
 
                 base_amount = applied_people_count * price_per_person_per_day * travel_days
                 formula_parts.append(f"{travel_days}일 * {applied_people_count}명 * {price_per_person_per_day:,}원")
+
+                if grade == "프리미엄":
+                    extra_amount += 200000
+                    formula_parts.append("(프리미엄 200,000원)")
 
             # 추가 금액
             if meal_excluded_count > 0:
@@ -251,6 +251,7 @@ if st.session_state.show_note_form:
     st.subheader("인솔자 쪽지 양식 작성")
     manager_name = st.text_input("상품 담당자 이름")
     visit_czech = st.checkbox("체코를 방문하는 상품인가요?")
+    visit_hallstatt = st.checkbox("할슈타트를 방문하는 상품인가요?")
 
     if st.button("실행"):
         czech_text = ""
@@ -261,6 +262,13 @@ if st.session_state.show_note_form:
 - 영문여행자 보험 증서 : 체코에서 여행자와 불법이민자 구분을 여행자보험 증서로 확인한다고 합니다.
 고객님들께 한부씩 전달 부탁드립니다. 현재 보험 시스템상 다른고객님들은 상관없는데 인솔자 등록 시스템 연동이 안돼서 인솔자님 성이 빠진 채 나옵니다.
 영문 보험증서만 성이 빠진 채 나오는 것이며, 국문은 정상으로 나오기 때문에 보험엔 문제 없음을 확인했습니다.
+"""
+
+        hallstatt_text = ""
+        if visit_hallstatt:
+            hallstatt_text = """
+
+6) 할슈타트 내 행사 진행 시에는 멘트는 꼭 버스 안에서만 진행해 주시고, 야외에서는 자유 일정으로 진행 부탁드립니다. 야외에서의 가이드 행위가 엄격히 금지되어 있는 지역으로, 가이드 분들이 현지 경찰에 인계되는 사례가 종종 발생하고 있습니다.
 """
 
         note_text = f"""인솔자님 안녕하세요, 하나투어 유럽상품1팀 {manager_name}입니다 :)
@@ -343,15 +351,15 @@ https://drive.google.com/drive/folders/1Tf_4RBZMWziPzlJBWKOim4HfNWok-4L6?usp=sha
 
 3) 물값관련
 - 하나투어는 물값, 매너팁 등의 명목으로 고객에게 공동경비외에 추가로 경비를 받지 않습니다
-- 타 지역 스페인 현지에서 협조요청사항이 한가지 있어서 이렇게 게시판에 공지올려드리니, 참고 부탁드리며, 이러한 일이 발생되지않도록 주의해주시기 바랍니다. 현재 스페인지역에서 중국식당, 일부 현지식당을 제외하고, 식사시 물값이 지불되고 있습니다. 최근 물값비용을 절약하기를 위해, 일부 인솔자님들께서 생수를 마트에서 미리 사두셨다가 식당에 들고 들어가서 세팅하는 사례가 빈번해지고 있다고 하며, 이 부분 때문에 코로나 이전에 식당과 로컬사무실의관계 및 업무적인 마찰을 초래했었습니다. 고객님들이 개인적으로 버스나, 가게에서 구입하신 생수를 소지하고 가실 수 는 있으나, 인솔자님들이 고의적으로 구입 후 셋팅하는건 상도에 어긋난다고 판단되며, 이로인하여, 현장에서 고객님들께 식당 측 으로의 클레임으로 인하여, 또 다른 불만을 초래할수 있는 상황이 발생되오니 꼭 주의 부탁드립니다.
+- 타 지역 스페인 현지에서 협조요청사항이 한가지 있어서 이렇게 게시판에 공지올려드리니, 참고 부탁드리며, 이러한 일이 발생되지않도록 주의해주시기 바랍니다. 현재 스페인지역에서 중국식당, 일부 현지식당을 제외하고, 식사시 물값이 지불되고 있습니다. 최근 물값비용을 절약하기를 위해, 일부 인솔자님들께서 생수를 마트에서 미리 사두셨다가 식당에 들고 들어가서 세팅하는 사례가 빈번해지고 있다고 하며, 이 부분 때문에 코로나 이전에 식당과 로컬사무실의관계 및 업무적인 마찰을 초래했었습니다. 고객님들이 개인적으로 버스나, 가게에서 구입하신 생수를 소지하고 가실 수 는 있으나, 인솔자님들이 고의적으로 구입 후 셋팅하는건 상도에 어긋난다고 판단되며, 이로 인하여, 현장에서 식당 측에서 고객님들께 항의하며, 또 다른 불만을 초래할수 있는 상황이 발생되오니 꼭 주의 부탁드립니다.
 
 4) 호텔객실키 추가반출 금지!!
 현지에서 도둑이 카운터에서 호텔 객실키를 발급 받아서 객실내로 침입한 사례가 있었습니다.
 카운터네 체크인시 절대 키 추가 반출 되지 않도록 주의주시길 부탁드립니다!
-손님들께는 번거로우시겠지만 키는 개별적으로 추가 생성은 불가한점 안내 부탁드립니다.
+손님들께는 번거로우시겠지만 키는 개별적으로 추가 생성은 불가한 점 안내 부탁드립니다.
 
 5)일정표에 없는 쇼핑이나, 옵션은 진행불가이며 하나투어 일정표와 현지 확정서가 다른지 꼭 확인 부탁드립니다. 
-작은 실수가 큰 손해로 이어지지 않도록, 확정표 상 이상하다고 생각되시는 부분 있으시면 꼭 말씀 부탁드리겠습니다.
+작은 실수가 큰 손해로 이어지지 않도록, 확정표 상 이상하다고 생각되시는 부분 있으시면 꼭 말씀 부탁드리겠습니다.{hallstatt_text}
 
 항상 현장에서 고군분투해주시는 인솔자님께 감사드립니다.
 
@@ -387,7 +395,7 @@ if st.session_state.generated_note_text:
             ">인솔자 쪽지 복사가 완료되었습니다!</span>
         </div>
 
-        <textarea id="noteText" style="width:100%; height:700px; padding:12px; font-size:14px;">{escaped_text}</textarea>
+        <textarea id="noteText" readonly style="width:100%; height:700px; padding:12px; font-size:14px;">{escaped_text}</textarea>
 
         <script>
         function copyNoteText() {{
